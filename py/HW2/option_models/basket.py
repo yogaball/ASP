55073b238926bb7baa15569a6adfb8cf3d791929
yogaball <jhyu.bupt@hotmail.com>
2022-03-18 14:58:19 +0800

    20220318_v1
    submit the hw2 for the version1

--- py/HW2/option_models/basket.py
+++ py/HW2/option_models/basket.py
@@ -57,11 +57,7 @@ def basket_price_mc_cv(
 
 def basket_price_mc(
     strike, spot, vol, weights, texp, cor_m,
-<<<<<<< HEAD
-    intr=0.0, divr=0.0, cp=1, bsm=True, n_samples = 10000
-=======
     intr=0.0, divr=0.0, cp=1, bsm=True, n_samples = 100000
->>>>>>> b8d5994cb5be7256617c0b900760028b4ffc6e69
 ):
     basket_check_args(spot, vol, cor_m, weights)
     
@@ -79,12 +75,7 @@ def basket_price_mc(
         '''
         PUT the simulation of the geometric brownian motion below
         '''
-<<<<<<< HEAD
-        #prices = forward[:,None]*np.exp(-1/2*vol[:,None]**2*texp + vol[:,None] * (np.sqrt(texp)*chol_m @ znorm_m))
         prices = forward[:,None]*np.exp(-1/2*vol[:,None]**2*texp +  (np.sqrt(texp)*chol_m @ znorm_m))
-=======
-        prices = np.zeros_like(znorm_m)
->>>>>>> b8d5994cb5be7256617c0b900760028b4ffc6e69
     else:
         # bsm = False: normal model
         prices = forward[:,None] + np.sqrt(texp) * chol_m @ znorm_m
@@ -106,28 +97,25 @@ def basket_price_norm_analytic(
     1. compute the forward of the basket
     2. compute the normal volatility of basket
     3. plug in the forward and volatility to the normal price formula
-<<<<<<< HEAD
-    normal_formula(strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1)
-    it is already imorted
-=======
     
     norm = pf.Norm(sigma, intr=intr, divr=divr)
     norm.price(strike, spot, texp, cp=cp)
->>>>>>> b8d5994cb5be7256617c0b900760028b4ffc6e69
     
     PUT YOUR CODE BELOW
     '''
     basket_check_args(spot, vol, cor_m, weights)
     
-<<<<<<< HEAD
     div_fac = np.exp(-texp*divr)
-    disc_fac = np.exp(-texp*intr)
-    forward = spot / disc_fac * div_fac
-    forward_basket = forward @ weights
+#     disc_fac = np.exp(-texp*intr)
+#     forward = spot / disc_fac * div_fac
+#     forward_basket = forward @ weights
+    spot_basket = spot @ weights
+    divr_basket = -np.log(div_fac @ weights)/texp
     cov_m = vol * cor_m * vol[:,None]
     sigma = np.sqrt(weights @ cov_m @ weights.T)
 
-    prices=normal_formula(strike, forward_basket, sigma, texp=texp, intr=intr, divr=divr, cp=1)
+    norm = pf.Norm(sigma, intr=intr, divr=divr_basket)
+    prices = norm.price(strike, spot_basket, texp, cp=cp)
     
     return prices
 
@@ -140,8 +128,3 @@ def spread_price_kirk(strike, spot, vol, texp, corr, intr=0, divr=0, cp=1):
     price = disc_fac * bsm_formula(forward[1]+strike, forward[0], vol_r, texp, cp=cp)
 
     return price
-=======
-    
-    
-    return 0.0
->>>>>>> b8d5994cb5be7256617c0b900760028b4ffc6e69
